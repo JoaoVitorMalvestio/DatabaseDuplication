fce69ea3ad9965ea69662786fc0f7a914378d03b
Michel Bolzon Souza Dos Reis <michel.bolzon123@gmail.com>
2020-11-16 22:36:32 -0300

    Update enum name

--- src/Enum/RouterEnum.java
+++ src/Enum/RouterEnum.java
@@ -3,10 +3,10 @@ package Enum;
 public enum RouterEnum {
     P1(8002, new RouterEnum[]{}),
     B1(8000, new RouterEnum[]{P1}),
-    C1(8003, new RouterEnum[]{P1}),
-    C2(8004, new RouterEnum[]{}),
-    W1(8005, new RouterEnum[]{}),
-    W2(8006, new RouterEnum[]{});
+    S1(8003, new RouterEnum[]{P1}),
+    S2(8004, new RouterEnum[]{}),
+    C1(8005, new RouterEnum[]{}),
+    C2(8006, new RouterEnum[]{});
 
     public int routerPort;
     public RouterEnum[] linkRouters;
